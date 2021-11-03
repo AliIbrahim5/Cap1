a9b680c61b65abf8cc5db7f0318e47787f5b068a
Aliibrahim5 <Ali.ibrahim.elq.js@tuwaiq.edu.sa>
2021-11-03 17:27:32 +0300

    home page

---
+++ hangman/src/components/Play/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './style.css'
+
+const Play = () => {
+    return (
+        <div className="container">
+        <div className="home">
+  
+            <h1>Play page</h1>
+        </div>
+        </div>
+    )
+}
+
+export default Play
