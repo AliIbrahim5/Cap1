d0f1ca9f581ebc760130a8de39e3c141c8c0a410
Aliibrahim5 <Ali.ibrahim.elq.js@tuwaiq.edu.sa>
2021-11-06 11:32:48 +0300

    l

--- src/components/Categories/index.js
+++ src/components/Categories/index.js
@@ -1,19 +1,32 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./style.css";
+import bttn from "../../image/button.mp3";
+import { FaArrowCircleLeft } from 'react-icons/fa';
+import { useHistory } from "react-router";
+
 
 const Categ = () => {
+  let history = useHistory();
+  const goBack = () => {
+    history.goBack();
+  };
   let bodyParts = ["NOSE", "TONGUE", "LEG", "HAND", "BACK"];
   let colors = ["RED", "BLACK", "BLUE", "WHITE", "PINK"];
   let animals = ["LION", "TIGER", "HORSE", "MONKEY", "CAT"];
   return (
     <div className="container">
+      <audio className="btn" autoPlay="autoplay" src={bttn}></audio>
       <div className="title">
         <p className="nameGameB">H</p> <p className="nameGameR">A</p>{" "}
         <p className="nameGameB">NGM</p>
         <p className="nameGameR">A</p> <p className="nameGameB">N</p>
       </div>
       <div className="home">
+      <p onClick={goBack} id="BackCateg">
+          <FaArrowCircleLeft />
+          Back
+        </p>
         <Link
           to={{ pathname: "/play", state: { myCateg: bodyParts } }}
           className="h1 lessSize"
